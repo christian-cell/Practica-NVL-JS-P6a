3d4f69352bd178f382d93be2c8221c79c7b7a23a
Christian-Cell <cristohp74@gmail.com>
2020-03-27 21:47:22 +0100

    sanp de la primera practica java script

---
+++ archivo.js
@@ -0,0 +1,17 @@
+var nombredelusuario = "christian";
+console.log(nombredelusuario)
+
+let apellidos = "Garcia martin";
+console.log(apellidos)
+
+let edad = "30años";
+console.log(edad)
+
+const losdiasdelasemana = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+console.log(losdiasdelasemana)
+
+var elvalordelagravedad = "La gravedad se podria definir 7 en escala de 10";
+console.log(elvalordelagravedad)
+
+const lostresprimerosnumerosimpares = [1,3,5];
+console.log(lostresprimerosnumerosimpares)
